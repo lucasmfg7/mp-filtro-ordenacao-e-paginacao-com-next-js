63aba85285e0d2c74892ec1477591d16134f0773
Lucas Fernandes <lucas_matheus22@hotmail.com>
2025-06-05 16:15:18 -0300

    feat: adiciona funcionalidade de busca dinâmica no componente SearchInput

--- components/search-input.tsx
+++ components/search-input.tsx
@@ -1,7 +1,24 @@
+'use client'
+
 import { Input } from '@/components/ui/input'
 import { Search } from 'lucide-react'
+import { usePathname, useRouter, useSearchParams } from 'next/navigation'
 
 export default function SearchInput() {
+  const searchParams = useSearchParams()
+  const pathName = usePathname()
+  const { replace } = useRouter()
+
+  function handleChange(e: React.ChangeEvent<HTMLInputElement>) {
+    const params = new URLSearchParams()
+    const search = e.currentTarget.value
+
+    if (search) params.set('search', search)
+    else params.delete('search')
+
+    replace(`${pathName}?${params.toString()}`)
+  }
+
   return (
     <div className="relative">
       <Search className="absolute left-2.5 top-2.5 h-4 w-4 text-muted-foreground" />
@@ -9,6 +26,7 @@ export default function SearchInput() {
         type="search"
         placeholder="Busque por nome..."
         className="w-full rounded-lg bg-background pl-8 md:w-[200px] lg:w-[336px]"
+        onChange={handleChange}
       />
     </div>
   )
